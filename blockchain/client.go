1bd00a2f6ebd499f19d053b3606599ad5ea14aa7
Daniel Carvalho <danielpacarvalho@gmail.com>
2024-03-26 10:54:45 -0300

    Change inital setup so that it shows balance

--- blockchain/client.go
+++ blockchain/client.go
@@ -1,15 +1,78 @@
 package blockchain
 
 import (
-	"log"
-
-	"github.com/ethereum/go-ethereum/ethclient"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 )
 
-func NewClient(url string) *ethclient.Client {
-	client, err := ethclient.Dial(url)
-	if err != nil {
-		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+type JSONRPCClient struct {
+	url string
+}
+
+func NewJSONRPCClient(url string) *JSONRPCClient {
+	return &JSONRPCClient{url: url}
+}
+
+type jsonRPCRequest struct {
+	JSONRPC string        `json:"jsonrpc"`
+	Method  string        `json:"method"`
+	Params  []interface{} `json:"params"`
+	ID      int           `json:"id"`
+}
+
+type jsonRPCResponse struct {
+	ID      int             `json:"id"`
+	JSONRPC string          `json:"jsonrpc"`
+	Result  json.RawMessage `json:"result"`
+	Error   *jsonRPCError   `json:"error"`
+}
+
+type jsonRPCError struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+// Error implements the error interface for jsonRPCError.
+func (e *jsonRPCError) Error() string {
+	return fmt.Sprintf("RPC Error %d: %s", e.Code, e.Message)
+}
+
+func (c *JSONRPCClient) Call(method string, params []interface{}) (json.RawMessage, error) {
+	request := jsonRPCRequest{
+		JSONRPC: "2.0",
+		Method:  method,
+		Params:  params,
+		ID:      1,
 	}
-	return client
+
+	requestBody, err := json.Marshal(request)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.Post(c.url, "application/json", bytes.NewBuffer(requestBody))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var response jsonRPCResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.Error != nil {
+		return nil, &jsonRPCError{Code: response.Error.Code, Message: response.Error.Message}
+	}
+
+	return response.Result, nil
 }
--- main.go
+++ main.go
@@ -1,23 +1,37 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"math/big"
 	"truswallet/blockchain"
-	"truswallet/store"
 )
 
 func main() {
-	client := blockchain.NewClient("https://mainnet.infura.io/v3/YOUR_PROJECT_ID")
-	storage := store.NewInMemoryStorage() // Initialize in-memory storage
+	rpcClient := blockchain.NewJSONRPCClient("https://cloudflare-eth.com")
 
-	// Example: Subscribing to an address
-	store.Subscribe("0xAddressHere")
-
-	// TODO: Adjusted call to ParseBlock with storage passed as an argument
-	err := blockchain.ParseBlock(client, 12345678, store.SubscribedAddresses, storage)
+	address := "0x71C7656EC7ab88b098defB751B7401B5f6d8976F"
+	result, err := rpcClient.Call("eth_getBalance", []interface{}{address, "latest"})
 	if err != nil {
-		log.Fatalf("Failed to parse block: %v", err)
+		log.Fatalf("Failed to get balance: %v", err)
 	}
 
-	// Optionally, you could retrieve and process transactions for a specific address from storage here...
+	var balanceHex string
+	err = json.Unmarshal(result, &balanceHex)
+	if err != nil {
+		log.Fatalf("Failed to unmarshal response: %v", err)
+	}
+
+	// Convert hexadecimal balance to a big.Int
+	balanceWei := new(big.Int)
+	balanceWei, ok := balanceWei.SetString(balanceHex[2:], 16) // Remove the "0x" prefix
+	if !ok {
+		log.Fatal("Failed to convert balance to big.Int")
+	}
+
+	// Convert Wei to ETH
+	weiToEth := new(big.Float).Quo(new(big.Float).SetInt(balanceWei), big.NewFloat(1e18))
+
+	fmt.Printf("Balance of address %s: %s ETH\n", address, weiToEth.Text('f', 18)) // 'f' for decimal point, 18 digits after decimal
 }
